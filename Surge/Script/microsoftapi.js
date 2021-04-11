19e3b19eea32e6d8f13da090cf3f18e2e4ec09a7
Apocalypsor <Apocalypsor@users.noreply.github.com>
2021-04-11 15:38:38 +0800

    Update microsoftapi.js

--- Surge/Script/microsoftapi.js
+++ Surge/Script/microsoftapi.js
@@ -1,6 +1,6 @@
 // URL = ^https:\/\/graph\.microsoft\.com\/v1\.0\/myorganization\/applications\/[\w-]+\/addPassword
 
-body = $response.body;
+body = $request.body;
 let obj = JSON.parse(body);
 obj["passwordCredential"]["endDateTime"] = "2999-10-11T07:19:47.610Z";
 body = JSON.stringify(obj); 
