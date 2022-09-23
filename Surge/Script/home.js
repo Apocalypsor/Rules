649af4ef097e92cdc045133e124bf0680668a2eb
Apocalypsor <37431235+Apocalypsor@users.noreply.github.com>
2022-09-22 21:11:24 -0400

    Update and rename homedns.js to home.js

---
+++ Surge/Script/home.js
@@ -0,0 +1,45 @@
+const ssid = "Verizon_B7MW9P";
+const dns_module = "Home";
+const http_api = "localhost:6171";
+const http_api_key = "Apocalypse";
+
+let request = {
+  url: `http://${http_api}/v1/modules`,
+  headers: {"X-Key": http_api_key},
+  body: {},
+}
+
+
+getModuleStatus(dns_module).then(main)
+
+
+function main(enable) {
+  let home = ($network.wifi.ssid == ssid);
+  if (home && !enable) {
+    //家里,未开启模块 => 开启
+    $notification.post("开启dns模块","","")
+    enableModule(true);
+  } else if (!home && enable){
+    //不是家里,开启了模块 => 关闭
+    $notification.post("关闭dns模块","","")
+    enableModule(false);
+  } else {
+    //重复触发 => 结束
+    //$notification.post("重复触发","","")
+    $done();
+    }
+}
+
+function getModuleStatus(dns_module) {
+  return new Promise(resolve => {
+    $httpClient.get(request, (error, response, data) => {
+      let enabled = JSON.parse(data).enabled;
+      resolve(enabled.includes(dns_module));
+    });
+  });
+}
+
+function enableModule(enable) {
+  request.body[dns_module] = enable;
+  $httpClient.post(request, () => $done());
+}
--- Surge/Script/homedns.js
+++
@@ -1,7 +0,0 @@
-if ($network.wifi.ssid === 'Verizon_B7MW9P') {
-    $done({
-        servers: '192.168.1.33'
-    })
-} else {
-    $done({})
-}
