3bcb4123ff28551aa052eb077601053eb10dd3f9
Apocalypsor <37431235+Apocalypsor@users.noreply.github.com>
2022-12-05 15:35:18 -0500

    Update slickdeals.js

--- Surge/Script/slickdeals.js
+++ Surge/Script/slickdeals.js
@@ -6,7 +6,13 @@ try {
         let newSections = []
         for (let section of obj.pages[pageNum].screen.sections) {
             if (section.containerType === 'ad') continue;
-            section.containers = section.containers.filter(item => !(item.value.flag && item.value.flag.flagText === 'PROMOTED'));
+            if (section.containers.filter(item => item.contentType === 'VersatileDealCard')) continue;
+            section.containers = section.containers.filter(item => {
+                if (item.value.flag && item.value.flag.flagText === 'PROMOTED') return false;
+                if (item.value.storeName.includes('Advertiser')) return false;
+                return true;
+            });
+            section.headline = null;
             newSections.push(section);
         }
         obj.pages[pageNum].screen.sections = newSections;
