e9553d24a5f50a31fb4029b5e2a233bcbdf6701a
Apocalypsor <chthon@cis.upenn.edu>
2024-03-12 22:45:31 -0400

    Update bilibili-cdn.user.js

--- UserScript/bilibili-cdn.user.js
+++ UserScript/bilibili-cdn.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         BiliBili CDN Optimizer
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  Optimize BiliBili CDN
 // @author       Apocalypsor
 // @match        https://*.bilibili.com/*
