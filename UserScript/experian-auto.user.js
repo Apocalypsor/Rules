b3a13debdab40741efb717d6b8fb21eedee33ae7
Apocalypsor <chthon605@gmail.com>
2024-05-29 11:02:35 +0800

    Add files via upload

---
+++ UserScript/experian-auto.user.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name         Experian AutoClicker
+// @namespace    http://tampermonkey.net/
+// @version      0.3
+// @description  Automatically clicks on the "No, Keep My Current Membership" button on Experian/Equifax.
+// @author       dov
+// @downloadURL  https://git.dov.moe/dov/Scripts/raw/branch/main/UserScript/experian-auto.user.js
+// @updateURL    https://git.dov.moe/dov/Scripts/raw/branch/main/UserScript/experian-auto.user.js
+// @match        https://usa.experian.com/mfe/member/preload
+// @match        https://usa.experian.com/mfe/member/loginInterstitial
+// @match        https://www.equifax.com/myequifax/loginsplashl/ecc
+// @grant        none
+// ==/UserScript==
+
+function clickExperianButton() {
+    const button = document.querySelector('button[data-ecs-tms="interstitial-login-decline"]');
+    if (button) {
+        button.click();
+    } else {
+        setTimeout(clickExperianButton, 500);
+    }
+}
+
+function clickEquifaxButton() {
+    const button = document.querySelector('#no');
+    if (button) {
+        button.click();
+    } else {
+        setTimeout(clickEquifaxButton, 500);
+    }
+}
+
+(function() {
+    'use strict';
+    const currentUrl = window.location.href;
+    console.log(currentUrl);
+
+    function autoclick() {
+        if (currentUrl === "https://usa.experian.com/mfe/member/loginInterstitial" || currentUrl === "https://usa.experian.com/mfe/member/preload") {
+            clickExperianButton();
+        } else if (currentUrl === "https://www.equifax.com/myequifax/loginsplashl/ecc") {
+            clickEquifaxButton();
+        }
+    }
+
+    autoclick();
+})();
